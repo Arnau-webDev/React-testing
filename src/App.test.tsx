6d56700ac80fa5f49669e05ecc371747734c6cf2
Arnau-webDev <arnauma16@gmail.com>
2023-09-20 19:48:08 +0200

    Code coverage

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,20 @@
-import { expect, test } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { describe, expect, test, vi } from 'vitest';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders whatever', () => {
-	render(<App />);
-	const headingElement = screen.getByText('Vite + React');
-	expect(headingElement).toBeInTheDocument();
+describe('App component', () => {
+	test('renders correctly', () => {
+		render(<App />);
+		const headingElement = screen.getByText('Vite + React');
+		expect(headingElement).toBeInTheDocument();
+	});
+
+	test('counter increases correctly', () => {
+		render(<App />);
+		const button = screen.getByRole('button');
+
+		fireEvent.click(button);
+	});
 });
 
 
--- src/components/greet/Greet.test.tsx
+++ src/components/greet/Greet.test.tsx
@@ -1,18 +1,20 @@
 import { render, screen } from '@testing-library/react';
-import { expect, test } from 'vitest';
+import { describe, expect, test } from 'vitest';
 import Greet from './Greet';
 
-test('Greet renders correctly', () => {
-	render(<Greet />);
-	const greetText = screen.getByText('Hello');
-
-	expect(greetText).toBeInTheDocument();
-});
-
-test('Greet renders with the provided name', () => {
-	const name = 'ArnauMQ';
-	render(<Greet name={name} />);
-	const greetText = screen.getByText(`Hello ${name}`);
-
-	expect(greetText).toBeInTheDocument();
+describe('Greet component', () => {
+	test('renders correctly', () => {
+		render(<Greet />);
+		const greetText = screen.getByText('Hello');
+    
+		expect(greetText).toBeInTheDocument();
+	});
+    
+	test('renders with the provided name', () => {
+		const name = 'ArnauMQ';
+		render(<Greet name={name} />);
+		const greetText = screen.getByText(`Hello ${name}`);
+    
+		expect(greetText).toBeInTheDocument;
+	});
 });
