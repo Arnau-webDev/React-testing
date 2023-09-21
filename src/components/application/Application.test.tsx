8db376f200c59e26dfc9a51740be322de4bf6754
Arnau-webDev <arnauma16@gmail.com>
2023-09-21 19:50:26 +0200

    getByRole options

--- src/components/application/Application.test.tsx
+++ src/components/application/Application.test.tsx
@@ -6,10 +6,27 @@ describe('Application', () => {
 	test('renders correctly', () => {
 		render(<Application />);
 
+		// getByRole should be your first option for selection DOM nodes in a test
+
+		// getByRoleOptions: name, level, hidden, selected, checked, pressed
+
+		const pageHeading = screen.getByRole('heading', { level: 1});
+
+		const pageSectionHeading = screen.getByRole('heading', { level: 2 });
+
+		const nameEl = screen.getByRole('textbox', { name: 'Name' });
+
+		const bioEl = screen.getByRole('textbox', { name: 'Bio' });
+
 		const jobLocationEl = screen.getByRole('combobox');
 		const termsEl = screen.getByRole('checkbox');
 		const submitBtnEl = screen.getByRole('button');
         
+		expect(pageHeading).toBeInTheDocument();
+		expect(pageSectionHeading).toBeInTheDocument();
+
+		expect(nameEl).toBeInTheDocument();
+		expect(bioEl).toBeInTheDocument();
 		expect(jobLocationEl).toBeInTheDocument();
 		expect(termsEl).toBeInTheDocument();
 		expect(submitBtnEl).toBeInTheDocument();
