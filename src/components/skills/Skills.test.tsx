ae2a49abca90a42e18a942b2f6d2e36d88231e5d
Arnau-webDev <arnauma16@gmail.com>
2023-09-22 18:56:17 +0200

    QueryByRole and fireEvent

--- src/components/skills/Skills.test.tsx
+++ src/components/skills/Skills.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { describe, expect, test } from 'vitest';
 import { Skills } from './Skills';
 
@@ -15,8 +15,33 @@ describe('Skills', () => {
 
 	test('renders a list of skills', () => {
 		render(<Skills skills={skills} />);
-
+		
 		const listItemElements = screen.getAllByRole('listitem');
 		expect(listItemElements).toHaveLength(skills.length);
+	});
+
+	test('renders Login button', () => {
+		render(<Skills skills={skills} />);
+
+		const loginBtn = screen.getByRole('button');
+		expect(loginBtn).toBeInTheDocument();
+	});
+
+	test('renders Start learning button after click', () => {
+		render(<Skills skills={skills} />);
+
+		const loginBtn = screen.getByRole('button');
+		expect(loginBtn.textContent).toBe('Login');
+
+		fireEvent.click(loginBtn);
+
+		expect(loginBtn.textContent).toBe('Start learning');
+	});
+
+	test('does not find Start Learning button in the DOM', () => {
+		render(<Skills skills={skills} />);
+
+		const loginBtn = screen.queryByRole('button', { name: 'Start learning'});
+		expect(loginBtn).not.toBeInTheDocument();
 	});
 });
