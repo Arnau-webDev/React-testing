d6e017666deebb1d962c0a60079ae0b810a10d6f
Arnau-webDev <arnauma16@gmail.com>
2023-09-24 17:50:57 +0200

    Add explanatory comments

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -12,7 +12,13 @@ import { server } from '../mocks/server';
   
 // Add `fetch` polyfill.
 global.fetch = fetch;
-  
+
+// Establish API mocking before all tests.
 beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
+
+// Reset any request handlers that we may add during the tests,
+// so they don't affect other tests.
 afterAll(() => server.close());
+
+// Clean up after the tests are finished.
 afterEach(() => server.resetHandlers());
