c3d50a57b0144771d86b86d0068cc0c7dd4e1ad1
Arnau-webDev <arnauma16@gmail.com>
2023-09-24 17:49:09 +0200

    Added fetch polyfill and config for msw

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,1 +1,18 @@
 import '@testing-library/jest-dom';
+
+// tests/setup.ts
+import {
+	afterAll,
+	afterEach,
+	beforeAll,
+} from 'vitest';
+import { fetch } from 'cross-fetch';
+  
+import { server } from '../mocks/server';
+  
+// Add `fetch` polyfill.
+global.fetch = fetch;
+  
+beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
+afterAll(() => server.close());
+afterEach(() => server.resetHandlers());
